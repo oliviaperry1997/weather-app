6fa5dc549e0113afa4d22e72cfa8ea642f20de17
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-03 19:52:50 +0100

    Add printWeather to print a weather report to DOM
    This commit refactors the code to use the new function printWeather,
    which takes the processedWeatherData object from the getWeather
    function in api-call.js and turn it into readable weather report that
    gets printed onto the DOM. It also tweaks the error handling.

--- src/api-call.js
+++ src/api-call.js
@@ -1,19 +1,16 @@
 export async function getWeather(location) {
-    try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=MD4EKCYT6BVLXJTMVYMCEUTHJ`)
-        const weatherData = await response.json();
-        const processedWeatherData = {
-            resolvedAddress: weatherData.resolvedAddress,
-            conditions: weatherData.currentConditions.conditions,
-            time: weatherData.currentConditions.datetime,
-            currentTemp: weatherData.currentConditions.temp,
-            todayMaxTemp: weatherData.days[0].tempmax,
-            todayMinTemp: weatherData.days[0].tempmin,
-            windSpeed: weatherData.currentConditions.windspeed,
-            windGust: weatherData.currentConditions.windgust,
-        }
-        console.log(processedWeatherData);
-    } catch (error) {
-        alert("Something went wrong.");
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=MD4EKCYT6BVLXJTMVYMCEUTHJ`)
+    const weatherData = await response.json();
+    const processedWeatherData = {
+        resolvedAddress: weatherData.resolvedAddress,
+        conditions: weatherData.currentConditions.conditions,
+        time: weatherData.currentConditions.datetime,
+        currentTemp: weatherData.currentConditions.temp,
+        todayMaxTemp: weatherData.days[0].tempmax,
+        todayMinTemp: weatherData.days[0].tempmin,
+        windDir: weatherData.currentConditions.winddir,
+        windSpeed: weatherData.currentConditions.windspeed,
+        windGust: weatherData.currentConditions.windgust,
     }
+    return processedWeatherData;
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./styles.css";
 import { getWeather } from "./api-call";
 
-const body = document.querySelector("body")
 const locationContainer = document.querySelector("#location-container")
-body.appendChild(locationContainer)
 
 const locationInput = document.createElement("input");
 locationInput.id = "location";
@@ -14,4 +12,79 @@ submit.textContent = "Submit"
 submit.setAttribute("type", "button");
 locationContainer.appendChild(submit);
 
-submit.addEventListener("click", () => getWeather(locationInput.value))
+async function printWeather(location) {
+    try {
+        const weather = await getWeather(location);
+        console.log(weather);
+        const weatherOutput = document.querySelector("#weather-output");
+        weatherOutput.textContent = "";
+
+        const weatherCond = document.createElement("p");
+        weatherCond.textContent = `The current weather for ${weather.resolvedAddress} as of ${weather.time} is ${weather.conditions}.`;
+        weatherOutput.appendChild(weatherCond);
+
+        const weatherTemp = document.createElement("p");
+        weatherTemp.textContent = `The current temperature is ${Math.floor(weather.currentTemp+0.5)}ºC, with a high of ${Math.floor(weather.todayMaxTemp+0.5)}ºC and a low of ${Math.floor(weather.todayMinTemp+0.5)}ºC.`;
+        weatherOutput.appendChild(weatherTemp);
+
+        const weatherWind = document.createElement("p");
+        let windDirection;
+        switch (Math.floor((weather.windDir+11.25)/22.5)) {
+            case 0:
+            case 16:
+                windDirection = "north";
+                break;
+            case 1:
+                windDirection = "north-northeast";
+                break;
+            case 2:
+                windDirection = "northeast";
+                break;
+            case 3:
+                windDirection = "east-northeast";
+                break;
+            case 4:
+                windDirection = "east";
+                break;
+            case 5:
+                windDirection = "east-southeast";
+                break;
+            case 6:
+                windDirection = "southeast";
+                break;
+            case 7:
+                windDirection = "south-southeast";
+                break;
+            case 8:
+                windDirection = "south";
+                break;
+            case 9:
+                windDirection = "south-southwest";
+                break;
+            case 10:
+                windDirection = "southwest";
+                break;
+            case 11:
+                windDirection = "west-southwest";
+                break;
+            case 12:
+                windDirection = "west";
+                break;
+            case 13:
+                windDirection = "west-northwest";
+                break;
+            case 14:
+                windDirection = "northwest";
+                break;
+            case 15:
+                windDirection = "north-northwest";
+                break;
+        }
+        weatherWind.textContent = `Wind is currently ${weather.windSpeed} km/h from the ${windDirection}, with gusts up to ${weather.windGust} km/h.`;
+        weatherOutput.appendChild(weatherWind);
+    } catch {
+        alert("Couldn't find that location.")
+    }
+}
+
+submit.addEventListener("click", () => printWeather(locationInput.value))
