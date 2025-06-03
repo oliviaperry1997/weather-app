2521676d04a2e74c76c9bdc0d0e2e514b6bb80e6
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-03 17:25:23 +0100

    Enhance getWeather to only return important info
    This commit enhances the getWeather function to process weatherData and
    return a trimmed down version that includes conditions, current
    temperature, wind speed, etc.

--- src/api-call.js
+++ src/api-call.js
@@ -2,7 +2,17 @@ export async function getWeather(location) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=MD4EKCYT6BVLXJTMVYMCEUTHJ`)
         const weatherData = await response.json();
-        console.log(weatherData);
+        const processedWeatherData = {
+            resolvedAddress: weatherData.resolvedAddress,
+            conditions: weatherData.currentConditions.conditions,
+            time: weatherData.currentConditions.datetime,
+            currentTemp: weatherData.currentConditions.temp,
+            todayMaxTemp: weatherData.days[0].tempmax,
+            todayMinTemp: weatherData.days[0].tempmin,
+            windSpeed: weatherData.currentConditions.windspeed,
+            windGust: weatherData.currentConditions.windgust,
+        }
+        console.log(processedWeatherData);
     } catch (error) {
         alert("Something went wrong.");
     }
