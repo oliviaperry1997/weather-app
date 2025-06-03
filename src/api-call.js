51a9a717f36205004cb06575e62d05d7e815176c
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-03 16:14:14 +0100

    Add error handling
    This commit adds simple error handling with a try...catch block in the
    getWeather function.

--- src/api-call.js
+++ src/api-call.js
@@ -1,5 +1,9 @@
 export async function getWeather(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=MD4EKCYT6BVLXJTMVYMCEUTHJ`)
-    const weatherData = await response.json();
-    console.log(weatherData);
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=MD4EKCYT6BVLXJTMVYMCEUTHJ`)
+        const weatherData = await response.json();
+        console.log(weatherData);
+    } catch (error) {
+        alert("Something went wrong.");
+    }
 }
