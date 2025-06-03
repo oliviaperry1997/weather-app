1f6d076429b3c689aef1ec5e0b78a1cec7f30f2e
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-03 14:50:07 +0100

    Add location input
    This commit adds an input field that allows the user to type in a
    location of their choosing and get that location's weather.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "./styles.css";
 import { getWeather } from "./api-call";
 
-getWeather("lisbon")
+const body = document.querySelector("body")
+const locationContainer = document.querySelector("#location-container")
+body.appendChild(locationContainer)
+
+const locationInput = document.createElement("input");
+locationInput.id = "location";
+locationContainer.appendChild(locationInput);
+
+const submit = document.createElement("button");
+submit.textContent = "Submit"
+submit.setAttribute("type", "button");
+locationContainer.appendChild(submit);
+
+submit.addEventListener("click", () => getWeather(locationInput.value))
